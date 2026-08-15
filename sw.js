70e66fbece57f2f608475482a713b9731622b559
Nathalia de Bona <nathalia.contactdc@gmail.com>
2026-08-14 21:28:19 -0300

    Add fetch event handler to service worker
    - Implement cache-first strategy in fetch event
    - Add catch fallback for failed requests outside cache

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'focus-flow-v1';
+const CACHE_NAME = 'focus-flow-v2';
 
 const filesToCache = [
     'index.html',
@@ -6,7 +6,10 @@ const filesToCache = [
     'script.js',
     'manifest.json',
     'assets/icons/icon-192.png',
-    'assets/icons/icon-512.png'
+    'assets/icons/icon-512.png',
+    'fonts/inter-regular.woff2',
+    'fonts/inter-medium.woff2',
+    'fonts/poppins-bold.woff2'
 ];
 
 self.addEventListener('install', (event) => {
@@ -15,4 +18,18 @@ self.addEventListener('install', (event) => {
             return cache.addAll(filesToCache);
         })
     );
-})
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((cached) => {
+            if (cached) {
+                return cached
+            } else {
+                return fetch(event.request)
+            };
+        }).catch(() => {
+            return new Response('', { status: 408})
+        })
+    );
+});
